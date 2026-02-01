8d03af6eedc4223ed262fe0a4fbba6954400c641
Rav1d <erikalvarez93081@gmail.com>
2026-01-31 18:55:29 -0600

    js - to-do app task input & delete tast function

---
+++ script.js
@@ -0,0 +1,32 @@
+const inputAdd = document.querySelector('.input-add');
+const btnAdd = document.querySelector('.btn-add');
+const listContainer = document.querySelector('.list-container');
+
+const addNewTask = (text) => {
+    const newListItem = document.createElement('li')
+    const newListElements =  document.createElement('div')
+    const newTextItem = document.createElement('p')
+    const newBtnDelete = document.createElement('button')
+
+    newTextItem.textContent = text
+    newBtnDelete.textContent = 'X'
+    newListElements.classList.add('list-elements')
+
+    newListElements.appendChild(newTextItem)
+    newListElements.appendChild(newBtnDelete)
+    newListItem.appendChild(newListElements)
+    listContainer.appendChild(newListItem)
+
+    newBtnDelete.addEventListener('click', (event) => {
+        if(event.target.tagName == 'BUTTON'){
+            newBtnDelete.parentElement.parentElement.remove();
+        }
+    })
+
+    inputAdd.value = ''
+}
+
+btnAdd.addEventListener('click', ()=>{
+    const texto = inputAdd.value
+    addNewTask(texto)
+})
