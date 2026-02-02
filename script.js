6b09c80c3d972e90df059ef085e73b5ac7605d51
Rav1d <erikalvarez93081@gmail.com>
2026-02-02 14:14:42 -0600

    js & html - added empty task message & validation

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 const inputAdd = document.querySelector('.input-add');
 const btnAdd = document.querySelector('.btn-add');
 const listContainer = document.querySelector('.list-container');
+const message = document.querySelector('.message')
 
 const addNewTask = (text) => {
-    const newListItem = document.createElement('li')
-    const newListElements =  document.createElement('div')
-    const newTextItem = document.createElement('p')
-    const newBtnDelete = document.createElement('button')
+    if(text.trim() == ''){
+        message.textContent = 'Cannot add empty tasks. Try again'
+    } else {
+        message.textContent = ''
+        const newListItem = document.createElement('li')
+        const newListElements =  document.createElement('div')
+        const newTextItem = document.createElement('p')
+        const newBtnDelete = document.createElement('button')
 
-    newTextItem.textContent = text
-    newBtnDelete.textContent = 'X'
-    newListElements.classList.add('list-elements')
+        newTextItem.textContent = text
+        newBtnDelete.textContent = 'X'
+        newListElements.classList.add('list-elements')
 
-    newListElements.appendChild(newTextItem)
-    newListElements.appendChild(newBtnDelete)
-    newListItem.appendChild(newListElements)
-    listContainer.appendChild(newListItem)
+        newListElements.appendChild(newTextItem)
+        newListElements.appendChild(newBtnDelete)
+        newListItem.appendChild(newListElements)
+        listContainer.appendChild(newListItem)
 
-    newBtnDelete.addEventListener('click', (event) => {
-        if(event.target.tagName == 'BUTTON'){
-            newBtnDelete.parentElement.parentElement.remove();
-        }
-    })
-
+        newBtnDelete.addEventListener('click', (event) => {
+            if(event.target.tagName == 'BUTTON'){
+                newBtnDelete.parentElement.parentElement.remove();
+            }
+        }) 
+    }
     inputAdd.value = ''
 }
 
