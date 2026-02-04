ef4440a8aa66b0a144d8c8e537770abdc80a7961
Rav1d <erikalvarez93081@gmail.com>
2026-02-04 12:18:03 -0600

    js - fixed bugs

--- script.js
+++ script.js
@@ -2,44 +2,49 @@ const inputAdd = document.querySelector('.input-add');
 const btnAdd = document.querySelector('.btn-add');
 const listContainer = document.querySelector('.list-container');
 const message = document.querySelector('.message')
-let taskDone = false
+
 const addNewTask = (text) => {
     if(text.trim() == ''){
         message.textContent = 'Cannot add empty tasks. Try again'
         inputAdd.classList.add('empty-input')
-    } else {
-        inputAdd.classList.remove('empty-input')
-        message.textContent = ''
-        const newListItem = document.createElement('li')
-        const newListElements =  document.createElement('div')
-        const newListButtons = document.createElement('div')
-        const newTextItem = document.createElement('p')
-        const newBtnDelete = document.createElement('button')
+        return
+    } 
+
+    inputAdd.classList.remove('empty-input')
+    message.textContent = ''
+    const newListItem = document.createElement('li')
+    const newListElements =  document.createElement('div')
+    const newListButtons = document.createElement('div')
+    const newTextItem = document.createElement('p')
+    const newBtnDelete = document.createElement('button')
+    const newBtnStatus = document.createElement('button')
         
-        newTextItem.textContent = text
-        newListItem.classList.add('list-item')
-        newBtnDelete.textContent = 'X'
-        newBtnDelete.classList.add('btn-delete')
+    newTextItem.textContent = text
+    newListItem.classList.add('list-item')
+    newBtnDelete.textContent = 'X'
+    newBtnDelete.classList.add('btn-delete')
+    newBtnStatus.textContent = 'Done'
+    newBtnStatus.classList.add('btn-status')
 
-        newListElements.classList.add('list-elements')
-        newListButtons.classList.add('list-buttons')
+    newListElements.classList.add('list-elements')
+    newListButtons.classList.add('list-buttons')
 
-        newListElements.appendChild(newTextItem)
+    newListElements.appendChild(newTextItem)
 
-        newListButtons.appendChild(newBtnDelete)
+    newListButtons.appendChild(newBtnStatus)
+    newListButtons.appendChild(newBtnDelete)
 
-        newListElements.appendChild(newListButtons)
-        newListItem.appendChild(newListElements)
-        listContainer.appendChild(newListItem)
+    newListElements.appendChild(newListButtons)
+    newListItem.appendChild(newListElements)
+    listContainer.appendChild(newListItem)
 
-        newBtnDelete.addEventListener('click', (event) => {
-            if(event.target.tagName == 'BUTTON'){
-                newBtnDelete.parentElement.parentElement.parentElement.remove();
-            }
-        }) 
-    }
+    newBtnDelete.addEventListener('click', () => {
+        newListItem.remove()
+    }) 
+
     inputAdd.value = ''
 }
+    
 
 btnAdd.addEventListener('click', ()=>{
     const texto = inputAdd.value
