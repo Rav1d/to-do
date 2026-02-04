a9a563dbe95ca983f7838cb37c68829291597c2f
Rav1d <erikalvarez93081@gmail.com>
2026-02-04 11:48:17 -0600

    js & css -  removed "done" feature

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const inputAdd = document.querySelector('.input-add');
 const btnAdd = document.querySelector('.btn-add');
 const listContainer = document.querySelector('.list-container');
 const message = document.querySelector('.message')
-let taskDone = true
+let taskDone = false
 const addNewTask = (text) => {
     if(text.trim() == ''){
         message.textContent = 'Cannot add empty tasks. Try again'
@@ -15,12 +15,9 @@ const addNewTask = (text) => {
         const newListButtons = document.createElement('div')
         const newTextItem = document.createElement('p')
         const newBtnDelete = document.createElement('button')
-        const newBtnDone = document.createElement('button');
         
         newTextItem.textContent = text
         newListItem.classList.add('list-item')
-        newBtnDone.textContent = 'Done'
-        newBtnDone.classList.add('btn-status')
         newBtnDelete.textContent = 'X'
         newBtnDelete.classList.add('btn-delete')
 
@@ -29,23 +26,11 @@ const addNewTask = (text) => {
 
         newListElements.appendChild(newTextItem)
 
-        newListButtons.appendChild(newBtnDone)
         newListButtons.appendChild(newBtnDelete)
 
         newListElements.appendChild(newListButtons)
         newListItem.appendChild(newListElements)
         listContainer.appendChild(newListItem)
-
-        newBtnDone.addEventListener('click', (event)=>{
-            if(event.target.tagName == 'BUTTON'){
-                if(taskDone != taskDone){
-                    newBtnDone.textContent = 'Done'
-                    newBtnDone.classList.add('btn-item-status')
-                } 
-                newBtnDone.textContent = 'Undo'
-                newBtnDone.classList.remove('btn-item-status')
-            } 
-        })
 
         newBtnDelete.addEventListener('click', (event) => {
             if(event.target.tagName == 'BUTTON'){
